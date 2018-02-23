9922ec04e6c6b99cf39f45e813ec754e1c5d3734
Delon Newman <dnewman20@cnm.edu>
2018-02-23 16:32:39 -0700

    added some more operations for sets and maps

--- src/main/java/edu/cnm/util/Utils.java
+++ src/main/java/edu/cnm/util/Utils.java
@@ -2,33 +2,80 @@ package edu.cnm.util;
 
 import java.util.*;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 
 public class Utils {
     // Set Operations
+    public static boolean isSet(Object o) {
+        if (o == null) return false;
+        return o instanceof Set;
+    }
+
     public static Set toSet(Collection c) {
         return new HashSet(c);
     }
 
     public static Set difference(Collection a, Collection b) {
-        Set c = new HashSet();
-        c.addAll(a);
+        Set c = new HashSet(a);
         c.removeAll(b);
         return c;
     }
 
     public static Set union(Collection a, Collection b) {
-        Set c = new HashSet();
-        c.addAll(a);
+        Set c = new HashSet(a);
         c.addAll(b);
         return c;
     }
 
     public static Set intersection(Collection a, Collection b) {
-        Set c = new HashSet();
-        c.addAll(a);
+        Set c = new HashSet(a);
         c.retainAll(b);
         return c;
+    }
+
+    // Map Operations
+    public static boolean isMap(Object o) {
+        if (o == null) return false;
+        return o instanceof Map;
+    }
+
+    public static Object get(Map m, Object k, Object alt) {
+        if (m == null || m.isEmpty()) return alt;
+        Object v = m.get(k);
+        if (v == null) return alt;
+        else {
+            return v;
+        }
+    }
+
+    public static Object get(Map m, Object k) {
+        return get(m, k, null);
+    }
+
+    public static Object getIn(Map m, List keys, Object alt) {
+        if (m == null || m.isEmpty()) return alt;
+        if (keys.size() == 1) {
+            return get(m, keys.get(0), alt);
+        }
+        else {
+            Object val = m;
+
+            for (Object key : keys) {
+                if (isMap(val)) {
+                    Object newVal = get((Map) val, key);
+                    if (newVal == null || (isMap(newVal) && ((Map) newVal).isEmpty())) {
+                        return alt;
+                    } else {
+                        val = newVal;
+                    }
+                }
+            }
+
+            return val;
+        }
+    }
+
+    public static Object getIn(Map m, List keys) {
+        return getIn(m, keys, null);
     }
 
     // Relational operations
@@ -47,7 +94,7 @@ public class Utils {
         return grouped;
     }
 
-    // a relational projection
+    // a relational projector
     public static Function<Map<String, Object>, Map<String, Object>> selectFields(Collection<String> keys) {
         Set<String> keys_ = new TreeSet<>(keys);
 
@@ -65,7 +112,7 @@ public class Utils {
         };
     }
 
-    // a negated relational projection
+    // a negated relational projector
     public static Function<Map<String, Object>, Map<String, Object>> rejectFields(Collection<String> keys) {
         Set<String> keys_ = new TreeSet<>(keys);
 
@@ -78,6 +125,20 @@ public class Utils {
                     proj.put(key, val);
                 }
 
+            }
+            return proj;
+        };
+    }
+
+    public static Function<Map<String, Object>, Map<String, Object>> mapFields(Map<String, String> map) {
+        return (Map<String, Object> record) -> {
+            Map<String, Object> proj = new TreeMap<>();
+            for (Map.Entry<String, Object> entry : record.entrySet()) {
+                String newKey = map.get(entry.getKey());
+                if (newKey == null) continue;
+                else {
+                    proj.put(newKey, entry.getValue());
+                }
             }
             return proj;
         };
